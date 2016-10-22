49da1bb91c89903a512d115c7668947d8a08f7fc
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-10-22 17:28:12 +0100

    Solarized colors

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Tamsyn:size=20" };
-static const char dmenufont[]       = "Tamsyn:size=20";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Source Code Pro:size=20" };
+static const char dmenufont[]       = "Source Code Pro:size=20";
+static const char col_gray1[]       = "#073642";
+static const char col_gray2[]       = "#fdf6e3";
+static const char col_gray3[]       = "#eee8d5";
+static const char col_gray4[]       = "#eee8d5";
+static const char col_cyan[]        = "#268bd2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
