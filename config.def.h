cfa74a97c6fd361a6d096e5cdf0d07772539c321
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-10-23 12:32:21 +0100

    Meta+w starts web browser (tabbed surf)

--- config.def.h
+++ config.def.h
@@ -59,6 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *webcmd[]  =  { "tabbed", "-c", "-d", "surf", "-z2", "-e", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 
 static const char *volupcmd[]   = { "amixer", "-q", "sset", "Master", "2%+", "unmute", NULL };
@@ -71,6 +72,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = webcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd} },
 	{      0,      XF86XK_AudioRaiseVolume,    spawn,          {.v = volupcmd } },
 	{      0,      XF86XK_AudioLowerVolume,    spawn,          {.v = voldowncmd } },
