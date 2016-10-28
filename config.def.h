13334d6a2809378ac768e57ea65fa323cf7ca5a2
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-10-29 00:25:49 +0100

    Change terminal's default title

--- config.def.h
+++ config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-t", "Terminal", NULL };
 static const char *webcmd[]  =  { "tabbed", "-c", "-d", "surf", "-z2", "-e", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 
